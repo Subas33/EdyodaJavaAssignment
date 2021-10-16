b7e7885d61c99174c9c2238e55a987314657531e
Subashchandrabose M <52975478+Subas33@users.noreply.github.com>
2021-10-16 23:27:33 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class LeapYear {
+    
+    
+    public boolean find(int year){
+        boolean leap;
+      if (year % 4 == 0) {
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                 leap = true;
+            else
+                leap = false;
+        }
+      
+        else
+            leap = true;
+        }
+    
+        else
+            leap = false;
+        
+        return leap;
+       }
+    
+    
+	public static void main (String[] args) {
+	    
+	    Scanner scan = new Scanner(System.in);
+		System.out.print("Enter Year : ");
+		int input = scan.nextInt();
+		Main solution = new Main();
+		if(solution.find(input)){
+		    System.out.println("Leap Year");;
+		}
+		else{
+		    System.out.println("Not Leap Year");;
+		}
+	}
+}
